c831ca49317bf0d12020dbd223ace4c4c34234bf
CBujeda <clarabujedamunoz@gmail.com>
2022-10-01 12:19:27 +0200

    klimakord 0.0.0

---
+++ src/main/java/com/cb/klimakord/klimakord/KlimakordApplication.java
@@ -0,0 +1,13 @@
+package com.cb.klimakord.klimakord;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KlimakordApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KlimakordApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cb/klimakord/klimakord/KlimakordApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cb.klimakord.klimakord;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KlimakordApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
